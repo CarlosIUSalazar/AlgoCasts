46e59d9d606101fd4418507a3bc8ad12be477750
Carlos Salazar <carlosiusalazar@gmail.com>
2020-08-27 10:36:24 +0900

    Reverse string test pass

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // SOLUTION 1
+    // let reverse = ''
+    // for (let i=str.length-1; i>=0; i--){
+    //     reverse = reverse + str[i]
+    //     console.log(str[i])
+    // }
+    // //console.log(reverse)
+    // return reverse;
+
+    // SOLUTION 2
+    return str.split("").reverse().join("")
+}
+
+reverse('abcd')
 
 module.exports = reverse;
